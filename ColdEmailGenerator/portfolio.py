75cca80e4d5e8f03ad9a251885f39e3d2268dbad
sabbir-063 <202114063@student.mist.ac.bd>
2025-08-06 21:30:46 +0600

    fixed path issues

--- ColdEmailGenerator/portfolio.py
+++ ColdEmailGenerator/portfolio.py
@@ -4,10 +4,10 @@ import uuid
 
 
 class Portfolio:
-    def __init__(self, file_path="resources/my_portfolio.csv"):
+    def __init__(self, file_path="ColdEmailGenerator/resources/my_portfolio.csv"):
         self.file_path = file_path
         self.data = pd.read_csv(file_path)
-        self.chroma_client = chromadb.PersistentClient('vectorstore')
+        self.chroma_client = chromadb.PersistentClient('ColdEmailGenerator/vectorstore')
         self.collection = self.chroma_client.get_or_create_collection(name="portfolio")
 
     def load_portfolio(self):
